7b52d37d3004e5e2be20b538ba74e237eb679dac
rectanglehihat <80513184+rectanglehihat@users.noreply.github.com>
2021-07-04 15:43:34 +0900

    firebase storage에 이미지 업로드

--- src/shared/Upload.js
+++ src/shared/Upload.js
@@ -1,11 +1,35 @@
 import React from "react";
+import {Button} from "../elements"
+import { storage } from "./firebase";
+
 
 const Upload = (props) => {
+    const fileInput = React.useRef();
+
+    const selectFile = (e) => {
+        console.log(e);
+        console.log(e.target);
+        console.log(e.target.files[0]);
+        console.log(fileInput.current.files[0]);
+    }
+
+    const uploadFB = () => {
+        let image = fileInput.current.files[0];
+        const _upload = storage.ref(`images/${image.name}`).put(image);
+
+        _upload.then((snapshot) => {
+            console.log(snapshot);
+
+            snapshot.ref.getDownloadURL().then((url) => {
+                console.log(url);
+            });
+          });
+    }
 
     return(
         <React.Fragment>
-            <input type="file"/>
-
+            <input type="file" onChange={selectFile} ref={fileInput}/>
+            <Button _onClick={uploadFB}>업로드하기</Button>
         </React.Fragment>
     )
 }
